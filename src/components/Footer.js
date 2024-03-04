dd6ae3b9f9244f0b2ecd507cf4d8d5a5978cd6db
malavrana1 <ranam211197@gmail.com>
2024-03-03 22:56:33 -0500

    Merge and pull all latest code change from Main to Develop

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../src/App.css'
+import '../../src/css/Footer.css'
+
+const Footer = () => {
+  return (
+    <p className="footer-container">
+      Designed & coded by{' '}
+      <a
+        href="https://github.com/malavrana1/React_Login_Page.git"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        MALAVRANA1
+      </a>
+    </p>
+  )
+}
+
+export default Footer
